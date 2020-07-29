c3eae3240faac1ffc174a36394d656729ffaf3b7
risingbirdsong <heregoingthere@outlook.com>
2020-07-29 10:17:13 -0700

    ah, i realized my sorting was mutating my working array, silly mistake, included reference for nice ways to sort on a copy

--- kWeakestRows.ts
+++ kWeakestRows.ts
@@ -1,11 +1,17 @@
 function kWeakestRows(mat: number[][], k: number): any {
-  return mat.sort((a, b) => {
-    let aReduced = a.reduce((acc, cur) => acc + cur);
-    let bReduced = b.reduce((acc, cur) => acc + cur);
-    return aReduced > bReduced ? 1 : aReduced < bReduced ? -1 : 0
-  }).filter((a, idx) => {
-    return idx < k ? true : false;
-  }).map(a => a.reduce((acc, cur) => acc + cur));
+  let reshaped = mat.map(arr => arr.reduce((a, c) => a + c));
+  // https://stackoverflow.com/questions/9592740/how-can-you-sort-an-array-without-mutating-the-original-array
+  let sorted = reshaped.sort((a, b) => a > b ? 1 : a < b ? -1 : 0);
+  let indexes = sorted.map((val) => {
+    console.log("val", val);
+    console.log("index of", reshaped.indexOf(val));
+    return reshaped.indexOf(val);
+  });
+  console.log('reshaped', reshaped);
+  console.log("sorted", sorted);
+  console.log("indexes", indexes);
+
+
 };
 
 let input = [[1, 1, 0, 0, 0],
