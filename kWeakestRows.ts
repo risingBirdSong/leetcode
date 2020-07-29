9d1481d7fec9d5261c231c830c6110545c87c892
risingbirdsong <heregoingthere@outlook.com>
2020-07-29 10:26:18 -0700

    creating copy for sorting, get the index of the sorted vals from the original reshaped array, then set the val to arbitray -1, to remove as a candidate so that if there are duplicate values, the duplicate vals will have unique indexes

--- kWeakestRows.ts
+++ kWeakestRows.ts
@@ -1,17 +1,12 @@
 function kWeakestRows(mat: number[][], k: number): any {
   let reshaped = mat.map(arr => arr.reduce((a, c) => a + c));
   // https://stackoverflow.com/questions/9592740/how-can-you-sort-an-array-without-mutating-the-original-array
-  let sorted = reshaped.sort((a, b) => a > b ? 1 : a < b ? -1 : 0);
+  let sorted = [...reshaped].sort((a, b) => a > b ? 1 : a < b ? -1 : 0);
   let indexes = sorted.map((val) => {
-    console.log("val", val);
-    console.log("index of", reshaped.indexOf(val));
-    return reshaped.indexOf(val);
-  });
-  console.log('reshaped', reshaped);
-  console.log("sorted", sorted);
-  console.log("indexes", indexes);
-
-
+    let idxOf = reshaped.indexOf(val);
+    reshaped[idxOf] = -1;
+    return idxOf;
+  }).filter((v, idx) => idx < k);
 };
 
 let input = [[1, 1, 0, 0, 0],
