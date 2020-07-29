efe99647a14e6b73bdc63c740979ff225562f592
risingbirdsong <heregoingthere@outlook.com>
2020-07-29 10:41:17 -0700

    my first solution for kWeakest, will make another attempt and look at others solutions

--- kWeakestRows.ts
+++ kWeakestRows.ts
@@ -2,7 +2,7 @@ function kWeakestRows(mat: number[][], k: number): any {
   let reshaped = mat.map(arr => arr.reduce((a, c) => a + c));
   // https://stackoverflow.com/questions/9592740/how-can-you-sort-an-array-without-mutating-the-original-array
   let sorted = [...reshaped].sort((a, b) => a > b ? 1 : a < b ? -1 : 0);
-  let indexes = sorted.map((val) => {
+  return sorted.map((val) => {
     let idxOf = reshaped.indexOf(val);
     reshaped[idxOf] = -1;
     return idxOf;
