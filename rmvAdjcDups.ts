25d7f886e1dcc74e6ec72198b23b7aadb6352977
risingbirdsong <heregoingthere@outlook.com>
2020-07-31 13:08:30 -0700

    this working solution was an effort to use recursion to remember the last splice posistion so as to not restart the loop at the beginning for more efficiency, however it isn't much of an improvement speed wise, curious about an iterative version of this

--- rmvAdjcDups.ts
+++ rmvAdjcDups.ts
@@ -1,11 +1,19 @@
 function removeDuplicates(s: string): string {
   const toArr = s.split('');
-  for (let i = 0; i < toArr.length; i++) {
-    if (toArr[i] === toArr[i + 1]) {
-      toArr.splice(i, 2);
-      i = -1;
+  const recurse = (idx: number, justDeleted: boolean) => {
+    for (let i = idx; i < toArr.length; i++) {
+      if (toArr[i] === toArr[i + 1]) {
+        toArr.splice(i, 2);
+        recurse(i, true)
+      }
+      if (justDeleted === true && toArr[i - 1] === toArr[i]) {
+        toArr.splice(i - 1, 2);
+        recurse(i, true);
+      }
+      justDeleted = false;
     }
   }
+  recurse(0, false);
   return toArr.join('');
 };
 
