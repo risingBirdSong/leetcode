0f7d90cf96e017f6e744413d83ed2c2013e08509
risingbirdsong <heregoingthere@outlook.com>
2020-08-04 15:52:22 -0700

    reverse a string

---
+++ reverseString.ts
@@ -0,0 +1,10 @@
+function reverseString(s: string[]): void {
+  for (let i = 0; i < s.length / 2; i++) {
+    let temp = s[i];
+    s[i] = s[s.length - 1 - i];
+    s[s.length - 1 - i] = temp;
+  }
+  console.log("rev", s);
+};
+
+console.log(reverseString(["h", "e", "l", "l", "o"]));
