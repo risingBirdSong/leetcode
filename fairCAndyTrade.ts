cc78702cf1047d84162097427f9cdef1aac6f2b0
risingbirdsong <heregoingthere@outlook.com>
2020-08-12 15:14:35 -0700

    a more efficient solution that passes the intesive tests, and fairly quickly too :)

--- fairCAndyTrade.ts
+++ fairCAndyTrade.ts
@@ -2,30 +2,27 @@ function numsSum(arr: number[]): number {
   return arr.reduce((a, c) => a + c);
 }
 
+interface trkI {
+  [keyof: number]: number;
+}
 
 function fairCandySwap(A: number[], B: number[]): number[] {
   let totalA = numsSum(A);
   let totalB = numsSum(B);
-  let even = (totalA + totalB) / 2;
-  for (let i = 0; i < A.length; i++) {
-    for (let j = 0; j < B.length; j++) {
-      let aCpy = [...A];
-      let bCPy = [...B];
-      let aCandidate = A[i];
-      let bCandidate = B[j];
-      let temp = A[i];
-      A[i] = B[j];
-      B[j] = temp;
-      if (numsSum(A) === numsSum(B)) {
-        return [aCandidate, bCandidate]
-      }
-      A = [...aCpy];
-      B = [...bCPy];
+  let evenMark = (totalA + totalB) / 2;
+  let diff = evenMark - numsSum(A);
+  let aTrk: trkI = {};
+  for (let num of A) {
+    aTrk[num + diff] = num;
+  }
+  for (let num of B) {
+    if (aTrk[num]) {
+      return [aTrk[num], num]
     }
   }
-  return [-1, -1];
+  throw Error("should never hit");
 };
 
 // A = [1,1], B = [2,2]
 console.log("fair candy", fairCandySwap([1, 2, 5], [2, 4]));
-// console.log("fair candy", fairCandySwap([2], [1, 3]));
+console.log("fair candy", fairCandySwap([1, 1], [2, 2]));
