799062620f58bf7e46dc04934283398c3fd29a31
risingbirdsong <heregoingthere@outlook.com>
2020-08-12 13:19:41 -0700

    solution looks to work but inefficient and times out

--- fairCAndyTrade.ts
+++ fairCAndyTrade.ts
@@ -2,34 +2,30 @@ function numsSum(arr: number[]): number {
   return arr.reduce((a, c) => a + c);
 }
 
+
 function fairCandySwap(A: number[], B: number[]): number[] {
   let totalA = numsSum(A);
   let totalB = numsSum(B);
-  let aCpy = [...A];
-  let bCpy = [...B];
-  let aSwap = -1;
-  let bSwap = -1;
-  outer:
-  for (let i = 0; i < aCpy.length; i++) {
-    for (let j = 0; j < bCpy.length; j++) {
-      aSwap = aCpy[i];
-      bSwap = bCpy[j];
-      let temp = aCpy[i];
-      aCpy[i] = bCpy[j];
-      bCpy[j] = temp;
-      totalA = numsSum(aCpy);
-      totalB = numsSum(bCpy);
-      if (totalA === totalB) {
-        console.log("here");
-        
-        break outer;
+  let even = (totalA + totalB) / 2;
+  for (let i = 0; i < A.length; i++) {
+    for (let j = 0; j < B.length; j++) {
+      let aCpy = [...A];
+      let bCPy = [...B];
+      let aCandidate = A[i];
+      let bCandidate = B[j];
+      let temp = A[i];
+      A[i] = B[j];
+      B[j] = temp;
+      if (numsSum(A) === numsSum(B)) {
+        return [aCandidate, bCandidate]
       }
-      console.log(totalA, totalB);
+      A = [...aCpy];
+      B = [...bCPy];
     }
   }
-  console.log("a cpy", aCpy, "b cpy", bCpy);
-  console.log("a swap", aSwap, "b swap", bSwap);
-  return [aSwap, bSwap];
+  return [-1, -1];
 };
 
-console.log("fair candy", fairCandySwap([2], [1, 3]));
+// A = [1,1], B = [2,2]
+console.log("fair candy", fairCandySwap([1, 2, 5], [2, 4]));
+// console.log("fair candy", fairCandySwap([2], [1, 3]));
