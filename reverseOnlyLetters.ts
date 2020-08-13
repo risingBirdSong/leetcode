b6e05c9928344889864d457e071ec223a47fc221
risingbirdsong <heregoingthere@outlook.com>
2020-08-13 07:44:33 -0700

    another more efficient idea that was fun to make

--- reverseOnlyLetters.ts
+++ reverseOnlyLetters.ts
@@ -4,7 +4,7 @@
 // handle reverse logic
 // but if it's not, keep the order
 
-function reverseOnlyLetters(S: string): string {
+function reverseOnlyLettersA(S: string): string {
   const onlyLtrs: string[] = [];
   const ltrReg = RegExp(/[a-z]/i);
   for (let str of S) {
@@ -26,4 +26,40 @@ function reverseOnlyLetters(S: string): string {
   return output.join("");
 };
 
-console.log(reverseOnlyLetters("a-bC-dEf-ghIj"));
+// console.log(reverseOnlyLettersA("a-bC-dEf-ghIj"));
+
+//this next idea was fun to experiment with and runs faster (although more complicated to think about)
+// because it only a has a single for loop and a lazy while loop (meaning it's just a partial iteration)
+// notLtrCount and nudeLeft are leftward offsets to account for only reversing letters.
+// notLtrCount is to account for left offsets for non letters our for loop has encountered
+// nudgeLeft is to account for non letters our for loop hasn't encountered yet.
+
+function reverseOnlyLetters(S: string): string {
+  let notLtrCount = 0;
+  let nudgeLeft = 0;
+  const onlyLtr = new RegExp(/[a-z]/i);
+  const output: string[] = [];
+  for (let i = 0; i < S.length; i++) {
+    if (onlyLtr.test(S[i])) {
+      let cur = S[i];
+      let reverseComplementIdx = S.length - i - 1 + notLtrCount - nudgeLeft;
+      let rvrsCompLtr = S[reverseComplementIdx];
+      while (!onlyLtr.test(S[reverseComplementIdx])) {
+        reverseComplementIdx--;
+        nudgeLeft++;
+      }
+      output[i] = S[reverseComplementIdx];
+    }
+    else {
+      notLtrCount++;
+      output[i] = S[i];
+    }
+  }
+  return output.join("");
+}
+//j-Ih-gfE-dCba 
+// console.log(reverseOnlyLetters("Test-ng-Leet-code-Q-"));
+console.log(reverseOnlyLetters("a1bc2d"));
+// console.log(reverseOnlyLetters("a-bC-dEf-ghIj"));
+
+
