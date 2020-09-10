b07b07dd958cb19ea2cb50383dac7221abff66a4
risingbirdsong <heregoingthere@outlook.com>
2020-09-10 10:24:54 -0700

    solved using circular travel, but I like the top solutions on leetcode better

--- distanceBetweenBusStops.ts
+++ distanceBetweenBusStops.ts
@@ -1,25 +1,35 @@
 function distanceBetweenBusStops(distance: number[], start: number, destination: number): number {
   let forwardDist = 0;
   let backwardsDist = 0;
-  for (let i = start; i < destination; i++) {
-    forwardDist += distance[i];
-  }
-  let idx = destination;
-  while (idx !== start) {
-    backwardsDist += distance[idx];
+  let idx = start;
+  //clockwise
+  while (idx !== destination) {
+    forwardDist += distance[idx];
     idx++;
     if (idx === distance.length) {
       idx = 0;
     }
   }
-  console.log("forward", forwardDist);
-  console.log("backward", backwardsDist);
+  //counterclock
+  idx = start;
+  while (idx !== destination) {
+    if (idx === 0) {
+      idx = distance.length;
+    }
+    idx--;
+    backwardsDist += distance[idx];
+  }  
   return [forwardDist, backwardsDist].sort((a, b) => a - b)[0];
 };
 
-// console.log("distance between", distanceBetweenBusStops([1, 2, 3, 4], 0, 1));
-// console.log("distance between", distanceBetweenBusStops([1, 2, 3, 4], 0, 2));
+// console.log("aaa", distanceBetweenBusStops([1, 2, 3, 4], 0, 1));
+// console.log("distance between", distanceBetweenBusStops([1, 2, 3, 4],
+//   0,
+//   2));
+console.log("distance between", distanceBetweenBusStops([1, 2, 3, 4],
+  0,
+  3));
 // console.log("distance between", distanceBetweenBusStops([7, 6, 3, 0, 3], 0, 4));
-console.log("distance between", distanceBetweenBusStops([7, 10, 1, 12, 11, 14, 5, 0],
-  7,
-  2));
+// console.log("distance between", distanceBetweenBusStops([7, 10, 1, 12, 11, 14, 5, 0],
+//   7,
+//   2));
