01e592f58850f2a9c892a399d2a1bcda19acf100
risingbirdsong <heregoingthere@outlook.com>
2020-09-10 19:02:17 -0700

    attempt at inserting, working so far :)

--- binarySearchTree/bst.ts
+++ binarySearchTree/bst.ts
@@ -10,6 +10,63 @@ class TreeNode {
   }
 }
 
+class BST {
+  constructor(public root: TreeNode | null) {
+    this.root = root;
+  }
+  insert(val: number) {
+    let newNode = new TreeNode(val);
+    if (this.root === null) {
+      this.root = newNode;
+    }
+    this.insertNode(this.root, newNode);
+  }
+  private insertNode(node: TreeNode, newNode: TreeNode) {
+    if (newNode.val === node.val) {
+      console.log("same value, ignore");
+      return false;
+    }
+    else if (newNode.val < node.val) {
+      if (!node.left) {
+        node.left = newNode;
+      }
+      this.insertNode(node.left, newNode)
+    }
+    else if (newNode.val > node.val) {
+      if (!node.right) {
+        node.right = newNode;
+      }
+      this.insertNode(node.right, newNode);
+    }
+  }
+  public inOrderPrint() {
+    if (!this.root) {
+      console.log("no root!");
+      return false;
+    }
+    const recurse = (node: TreeNode) => {
+      if (node.left) {
+        recurse(node.left)
+      }
+      console.log("val->", node.val);
+      if (node.right) {
+        recurse(node.right);
+      }
+    }
+    recurse(this.root)
+  }
+}
+
+const bst = new BST(new TreeNode(10));
+bst.inOrderPrint();
+bst.insert(9);
+bst.insert(11);
+bst.insert(8);
+bst.insert(12);
+bst.insert(7);
+bst.insert(13);
+bst.inOrderPrint();
+
 
 function insertLevelOrder(numbers: number[], root: TreeNode | null, i: number) {
   if (i < numbers.length) {
@@ -21,7 +78,9 @@ function insertLevelOrder(numbers: number[], root: TreeNode | null, i: number) {
   return root
 }
 
-let treeA = new TreeNode();
-let numsA = [1, 2, 3, 4, 5, 6, 6, 6, 6, 6];
-let testA = insertLevelOrder(numsA, treeA, 0);
-console.log("test A", testA);
+
+// let treeA = new TreeNode();
+// let numsA = [1, 2, 3, 4, 5, 6, 6, 6, 6, 6];
+// let testA = insertLevelOrder(numsA, treeA, 0);
+// console.log("test A", testA);
+
